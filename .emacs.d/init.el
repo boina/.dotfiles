29daa5ba77ac7061b3cef62b77e5dccdb8897933
José <jose.wo@posteo.net>
2024-09-27 17:36:34 +0200

    Updated pubmed Elfeed links.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -107,10 +107,10 @@
   :ensure t
   :config
   (setq elfeed-feeds
-	'(("https://pubmed.ncbi.nlm.nih.gov/rss/search/18cpB6wjstdmh5fOeH_LHYpkBlQueZvAqbyU5xXlOiV1IeWAuI/?limit=20&utm_campaign=pubmed-2&fc=20240324073238" Mucin)
-	  ("https://pubmed.ncbi.nlm.nih.gov/rss/search/1XigQ8Atc1EM7s1gj0F9R43DUx6icFEXNHE7_t2eM8pST9icKm/?limit=15&utm_campaign=pubmed-2&fc=20240902110758" Tspan8)
-	  ("https://pubmed.ncbi.nlm.nih.gov/rss/search/1FQ_Hz0ueKYrijPaRUpMMQuYAYxoWeGMFRRH-0rfCObDF-xEps/?limit=15&utm_campaign=pubmed-2&fc=20240902110510" Tspan3)
-	  ("https://pubmed.ncbi.nlm.nih.gov/rss/search/1zSVwQViw4hqkZqSLMwenjWRxtjin29VO1t49N4USbVyqxhMJ8/?limit=20&utm_campaign=pubmed-2&fc=20240903072709" ERES)
+	'(("https://pubmed.ncbi.nlm.nih.gov/rss/search/18cpB6wjstdmh5fOeH_LHYpkBlQueZvAqbyU5xXlOiV1IeWAuI/?limit=20&utm_campaign=pubmed-2&fc=20240324073238" PubMed Mucin)
+	  ("https://pubmed.ncbi.nlm.nih.gov/rss/search/1XigQ8Atc1EM7s1gj0F9R43DUx6icFEXNHE7_t2eM8pST9icKm/?limit=15&utm_campaign=pubmed-2&fc=20240902110758" PubMed Tspan8)
+	  ("https://pubmed.ncbi.nlm.nih.gov/rss/search/1FQ_Hz0ueKYrijPaRUpMMQuYAYxoWeGMFRRH-0rfCObDF-xEps/?limit=15&utm_campaign=pubmed-2&fc=20240902110510" PubMed Tspan3)
+	  ("https://pubmed.ncbi.nlm.nih.gov/rss/search/1zSVwQViw4hqkZqSLMwenjWRxtjin29VO1t49N4USbVyqxhMJ8/?limit=20&utm_campaign=pubmed-2&fc=20240903072709" PubMed ERES)
 	  ("http://connect.biorxiv.org/biorxiv_xml.php?subject=cell_biology" bioRxiv CellBiology)))
   (setq elfeed-search-title-max-width 180)
   (setq elfeed-db-directory "~/Nextcloud/.elfeed/"))
